af394b911e03582a80ec3c1cef26cd7855e23ffe
Gaute Meek Olsen <gaute.meek@gmail.com>
2023-12-05 20:00:58 +0100

    day5

--- src/main/kotlin/day5.kt
+++ src/main/kotlin/day5.kt
@@ -1,7 +1,4 @@
 import shared.*
-import kotlin.math.pow
-
-/* WARNING not cleaned up yet */
 
 fun main() {
     val input = Input.day(5)
@@ -11,62 +8,50 @@ fun main() {
 
 fun day5A(input: Input): Long {
     val seeds = input.chunks[0].toLongs()
-    val maps = input.chunks.drop(1).map { chunk ->
-        MapConverter(
-            chunk.lines().drop(1).map {
-                val (destination, source, range) = it.toLongs()
-                Converter(destination, source, range)
-            }
-        )
-    }
+    val categories = input.chunks.drop(1).map(Category::from)
     val mapped = seeds.map { seed ->
-        var value = seed
-        maps.forEach {
-            value = it.map(value)
+        categories.fold(seed) { value, category ->
+            category.map(value)
         }
-        value
     }
     return mapped.min()
 }
 
 fun day5B(input: Input): Long {
-    val seeds = input.chunks[0].toLongs().chunked(2)
-    val maps = input.chunks.drop(1).map { chunk ->
-        MapConverter(
-            chunk.lines().drop(1).map {
-                val (destination, source, range) = it.toLongs()
-                Converter(destination, source, range)
-            }
-        )
-    }
     var min = Long.MAX_VALUE
-    seeds.forEach {
-        var x = it[0]
-        while (x < it[0] + it[1]) {
-            var value = x
-            maps.forEach {
-                value = it.map(value)
+    val seeds = input.chunks[0].toLongs().chunked(2)
+    val categories = input.chunks.drop(1).map(Category::from)
+    seeds.forEach { seed ->
+        for (i in seed[0]..<seed[0] + seed[1]) {
+            val value = categories.fold(i) { value, category ->
+                category.map(value)
             }
             min = minOf(min, value)
-            x++
         }
     }
     return min
 }
 
-class MapConverter(val converters: List<Converter>) {
-    fun map(from: Long): Long {
-        return converters.firstOrNull { it.inRange(from) }?.map(from) ?: from
+private class Category(val converters: List<Converter>) {
+    fun map(value: Long): Long {
+        return converters.firstOrNull { it.inRange(value) }?.mappedTo(value) ?: value
+    }
+
+    companion object {
+        fun from(chunk: String): Category {
+            return Category(chunk.lines().drop(1).map(Converter::from))
+        }
     }
 }
 
-class Converter(val destination: Long, val source: Long, val range: Long) {
-    fun inRange(from: Long) = from >= source && from < source + range
+private class Converter(val destination: Long, val source: Long, val range: Long) {
+    fun inRange(value: Long) = value >= source && value < source + range
+    fun mappedTo(value: Long) = destination + value - source
 
-    fun map(from: Long): Long? {
-        if(from >= source && from < source + range) {
-            return destination + from - source
+    companion object {
+        fun from(line: String): Converter {
+            val (destination, source, range) = line.toLongs()
+            return Converter(destination, source, range)
         }
-        return null
     }
 }
