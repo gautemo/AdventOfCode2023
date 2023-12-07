fe23a9e6cb1c2c2d21972024356aaeee7bf438e2
Gaute Meek Olsen <gaute.meek@gmail.com>
2023-12-07 21:41:04 +0100

    day7

--- src/main/kotlin/day7.kt
+++ src/main/kotlin/day7.kt
@@ -1,7 +1,5 @@
 import shared.*
 
-/* WARNING not cleaned up */
-
 fun main() {
     val input = Input.day(7)
     println(day7A(input))
@@ -10,117 +8,79 @@ fun main() {
 
 fun day7A(input: Input): Int {
     val hands = input.lines.map { Hand(it) }
-    return hands.sorted().mapIndexed { index, hand ->
-        hand.bid * (index + 1)
-    }.sum()
+    return totalWinnings(hands)
 }
 
 fun day7B(input: Input): Int {
-    val hands = input.lines.map { Hand2(it) }
+    val hands = input.lines.map { Hand(it, true) }
+    return totalWinnings(hands)
+}
+
+private fun totalWinnings(hands: List<Hand>): Int {
     return hands.sorted().mapIndexed { index, hand ->
-        println("${hand.hand} (${hand.type}): ${hand.bid} * ${index + 1}")
         hand.bid * (index + 1)
     }.sum()
 }
-// 246167776 too low
-// 246971320 too high
 
-private class Hand(input: String): Comparable<Hand> {
+private class Hand(input: String, private val jokerMode: Boolean = false): Comparable<Hand> {
     val hand = input.split(' ')[0]
     val bid = input.split(' ')[1].toInt()
-    val type: Type
-    private val cardValues = mapOf(
-        'T' to 10,
-        'J' to 11,
-        'Q' to 12,
-        'K' to 13,
-        'A' to 15,
-    )
+    val type = if(jokerMode) Type.fromWithJokers(hand) else Type.from(hand)
 
-    init {
-        val times = hand.groupBy { it }.map { it.value.size }
-        type = when {
-            times.contains(5) -> Type.FiveOfAKind
-            times.contains(4) -> Type.FourOfAKind
-            times.contains(3) && times.contains(2) -> Type.FullHouse
-            times.contains(3) -> Type.ThreeOfAKind
-            times.count { it == 2 } == 2 -> Type.TwoPair
-            times.contains(2) -> Type.OnePair
-            else -> Type.HighCard
+    private fun Char.value(): Int {
+        return when(this) {
+            'T' -> 10
+            'J' -> if(jokerMode) 1 else 11
+            'Q' -> 12
+            'K' -> 13
+            'A' -> 15
+            else -> digitToInt()
         }
     }
 
     override fun compareTo(other: Hand): Int {
-        val typeCompare = type.value.compareTo(other.type.value)
-        if(typeCompare != 0) {
-            return typeCompare
+        type.sortValue.compareTo(other.type.sortValue).let {
+            if(it != 0) return it
         }
-        val cardsCompared = hand.mapIndexed { index, c ->
-            val value = cardValues[c] ?: c.digitToInt()
-            val otherValue = cardValues[other.hand[index]] ?: other.hand[index].digitToInt()
-            value.compareTo(otherValue)
+        hand.forEachIndexed { index, c ->
+            c.value().compareTo(other.hand[index].value()).let {
+                if(it != 0) return it
+            }
         }
-        return cardsCompared.firstOrNull { it != 0 } ?: 0
+        return 0
     }
 }
 
-private enum class Type(val value: Int) {
-    FiveOfAKind(7),
-    FourOfAKind(6),
-    FullHouse(5),
-    ThreeOfAKind(4),
-    TwoPair(3),
-    OnePair(2),
-    HighCard(1),
-}
+private enum class Type(val nextWithJoker: Type?, val sortValue: Int) {
+    FiveOfAKind(null, 7),
+    FourOfAKind(FiveOfAKind, 6),
+    FullHouse(FourOfAKind, 5),
+    ThreeOfAKind(FourOfAKind, 4),
+    TwoPair(FullHouse, 3),
+    OnePair(ThreeOfAKind, 2),
+    HighCard(OnePair, 1);
 
-private class Hand2(input: String): Comparable<Hand2> {
-    val hand = input.split(' ')[0]
-    val bid = input.split(' ')[1].toInt()
-    val type: Type
-    private val cardValues = mapOf(
-        'T' to 10,
-        'J' to 1,
-        'Q' to 12,
-        'K' to 13,
-        'A' to 15,
-    )
+    companion object {
+        fun from(hand: String): Type {
+            val times = hand.groupBy { it }.map { it.value.size }
+            return when {
+                times.contains(5) -> FiveOfAKind
+                times.contains(4) -> FourOfAKind
+                times.contains(3) && times.contains(2) -> FullHouse
+                times.contains(3) -> ThreeOfAKind
+                times.count { it == 2 } == 2 -> TwoPair
+                times.contains(2) -> OnePair
+                else -> HighCard
+            }
+        }
 
-    init {
-        val times = hand.filter { it != 'J' }.groupBy { it }.map { it.value.size }
-        val jokers = hand.count { it == 'J' }
-        type = when {
-            times.contains(5) || jokers == 5 -> Type.FiveOfAKind
-            times.contains(4) && jokers == 1 -> Type.FiveOfAKind
-            times.contains(3) && jokers == 2 -> Type.FiveOfAKind
-            times.contains(2) && jokers == 3 -> Type.FiveOfAKind
-            jokers == 4 -> Type.FiveOfAKind
-            times.contains(4) -> Type.FourOfAKind
-            times.contains(3) && jokers == 1 -> Type.FourOfAKind
-            times.contains(2) && jokers == 2 -> Type.FourOfAKind
-            jokers == 3 -> Type.FourOfAKind
-            times.contains(3) && times.contains(2) -> Type.FullHouse
-            times.count { it == 2 } == 2 && jokers == 1 -> Type.FullHouse
-            times.contains(3) -> Type.ThreeOfAKind
-            jokers == 2 -> Type.ThreeOfAKind
-            times.contains(2) && jokers == 1 -> Type.ThreeOfAKind
-            times.count { it == 2 } == 2 -> Type.TwoPair
-            times.contains(2) -> Type.OnePair
-            jokers == 1 -> Type.OnePair
-            else -> Type.HighCard
+        fun fromWithJokers(hand: String): Type {
+            if(hand == "JJJJJ") return FiveOfAKind
+            var type = from(hand.filter { it != 'J' })
+            repeat(hand.count { it == 'J' }) {
+                type = type.nextWithJoker ?: throw Exception("FiveOfAKind can't have more jokers")
+            }
+            return type
         }
-    }
-
-    override fun compareTo(other: Hand2): Int {
-        val typeCompare = type.value.compareTo(other.type.value)
-        if(typeCompare != 0) {
-            return typeCompare
-        }
-        val cardsCompared = hand.mapIndexed { index, c ->
-            val value = cardValues[c] ?: c.digitToInt()
-            val otherValue = cardValues[other.hand[index]] ?: other.hand[index].digitToInt()
-            value.compareTo(otherValue)
-        }
-        return cardsCompared.firstOrNull { it != 0 } ?: 0
     }
 }
