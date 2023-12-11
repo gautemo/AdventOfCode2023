658f102a37f791f07a3012ee31b6f76f045e71f7
Gaute Meek Olsen <gaute.meek@gmail.com>
2023-12-11 19:53:38 +0100

    day10

--- src/main/kotlin/day10.kt
+++ src/main/kotlin/day10.kt
@@ -2,8 +2,6 @@ import shared.Input
 import shared.Point
 import shared.XYMap
 
-/* WARNING messy code, not cleaned up yet */
-
 fun main() {
     val input = Input.day(10)
     println(day10A(input))
@@ -12,53 +10,19 @@ fun main() {
 
 fun day10A(input: Input): Int {
     val map = XYMap(input) { if (it == '.') null else it }
-    val trail = mutableListOf(map.all { it == 'S' }.keys.first())
-    while (true) {
-        val on = trail.last()
-        var next: Point? = null
-        Point(on.x, on.y - 1).let { top ->
-            if (
-                listOf('S', '|', 'L', 'J').contains(map[on]) &&
-                listOf('S', '|', '7', 'F').contains(map.getOrNull(top)) &&
-                !trail.contains(top)
-            ) {
-                next = top
-            }
-        }
-        Point(on.x + 1, on.y).let { right ->
-            if (
-                listOf('S', '-', 'L', 'F').contains(map[on]) &&
-                listOf('S', '-', '7', 'J').contains(map.getOrNull(right)) &&
-                !trail.contains(right)
-            ) {
-                next = right
-            }
-        }
-        Point(on.x, on.y + 1).let { bottom ->
-            if (
-                listOf('S', '|', '7', 'F').contains(map[on]) &&
-                listOf('S', '|', 'L', 'J').contains(map.getOrNull(bottom)) &&
-                !trail.contains(bottom)
-            ) {
-                next = bottom
-            }
-        }
-        Point(on.x - 1, on.y).let { left ->
-            if (
-                listOf('S', '-', '7', 'J').contains(map[on]) &&
-                listOf('S', '-', 'L', 'F').contains(map.getOrNull(left)) &&
-                !trail.contains(left)
-            ) {
-                next = left
-            }
-        }
-        if(next == null) return trail.size / 2
-        next?.let { trail.add(it) }
-    }
+    val trail = getTrail(map)
+    return trail.size / 2
 }
 
+/* next time, just be smart and count if number of pipes to the right is odd */
 fun day10B(input: Input): Int {
     val map = XYMap(input) { if (it == '.') null else it }
+    val trail = getTrail(map)
+    val expandedMap = getExpandedEmptySpaces(trail, map)
+    return getClosedPoints(expandedMap)
+}
+
+private fun getTrail(map: XYMap<Char>): List<Point> {
     val trail = mutableListOf(map.all { it == 'S' }.keys.first())
     while (true) {
         val on = trail.last()
@@ -99,16 +63,20 @@ fun day10B(input: Input): Int {
                 next = left
             }
         }
-        if(next == null) break
+        if (next == null) break
         next?.let { trail.add(it) }
     }
+    return trail
+}
+
+private fun getExpandedEmptySpaces(trail: List<Point>, map: XYMap<Char>): XYMap<Char> {
     var expandedInput = ""
-    for(y in 0 ..< map.height) {
-        for(x in 0 ..< map.width) {
-            if(trail.contains(Point(x, y))) {
+    for (y in 0..<map.height) {
+        for (x in 0..<map.width) {
+            if (trail.contains(Point(x, y))) {
                 expandedInput += map[Point(x, y)]
                 val connectsRight = getNeighbours(Point(x, y), trail).contains(Point(x + 1, y))
-                expandedInput += if(connectsRight) {
+                expandedInput += if (connectsRight) {
                     "-"
                 } else {
                     "e"
@@ -118,10 +86,10 @@ fun day10B(input: Input): Int {
             }
         }
         expandedInput += '\n'
-        for(x in 0 ..< map.width) {
-            expandedInput += if(trail.contains(Point(x, y))) {
+        for (x in 0..<map.width) {
+            expandedInput += if (trail.contains(Point(x, y))) {
                 val connectsBottom = getNeighbours(Point(x, y), trail).contains(Point(x, y + 1))
-                if(connectsBottom) {
+                if (connectsBottom) {
                     "|e"
                 } else {
                     "ee"
@@ -132,54 +100,46 @@ fun day10B(input: Input): Int {
         }
         expandedInput += '\n'
     }
-    println(expandedInput)
-    val expandedMap = XYMap(Input(expandedInput.trim())) { if(it == '.' || it == 'e') it else null }
-    var count = 0
-    val knownEscapes = mutableSetOf<Point>()
-    val knownTrapped = mutableSetOf<Point>()
-    for(it in expandedMap.all()) {
-        if(it.value == '.') {
-            val answer = canEscape(it.key, expandedMap, knownEscapes.toList(), knownTrapped.toList())
-            if(answer.second) {
-                knownEscapes.addAll(answer.first)
-            } else {
-                knownTrapped.addAll(answer.first)
-                count++
-            }
-        }
-    }
-    return count
+    return XYMap(Input(expandedInput.trim())) { if (it == '.' || it == 'e') it else null }
 }
 
 private fun getNeighbours(point: Point, list: List<Point>): List<Point> {
     val index = list.indexOf(point)
-    if(index == 0) {
+    if (index == 0) {
         return listOf(list.last(), list[1])
     }
-    if(index == list.size - 1) {
+    if (index == list.size - 1) {
         return listOf(list[index - 1], list[0])
     }
     return listOf(list[index - 1], list[index + 1])
 }
 
-private fun canEscape(point: Point, map: XYMap<Char>, knownEscapes: List<Point>, knownTrapped: List<Point>): Pair<List<Point>, Boolean> {
-    println("on y ${point.y} of ${map.height-1}")
-    val visited = mutableListOf<Point>()
-    val options = mutableSetOf(point)
-    while (options.isNotEmpty()) {
-        val on = options.first()
-        if(knownEscapes.contains(on)) {
-            return visited to true
+fun getClosedPoints(map: XYMap<Char>): Int {
+    val knownEscapes = mutableListOf<Point>()
+    val knownTrapped = mutableListOf<Point>()
+
+    fun checkEscape(point: Point): Boolean {
+        val options = mutableSetOf(point)
+        val visited = mutableListOf<Point>()
+        while (options.isNotEmpty()) {
+            val on = options.first()
+            options.remove(on)
+            visited.add(on)
+            if (knownTrapped.contains(on)) break
+            if (knownEscapes.contains(on) ||
+                on.x == 0 ||
+                on.y == 0 ||
+                on.x == map.width - 1 ||
+                on.y == map.height - 1
+            ) {
+                knownEscapes.addAll(visited + options)
+                return true
+            }
+            options.addAll(map.adjacents(on).keys.filter { !visited.contains(it) })
         }
-        if(knownTrapped.contains(on)) {
-            return visited to false
-        }
-        if(on.x == 0 || on.y == 0 || on.x == map.width - 1 || on.y == map.height - 1) {
-            return visited to true
-        }
-        options.addAll(map.adjacents(on).keys.filter { !visited.contains(it) })
-        visited.add(on)
-        options.remove(on)
+        knownTrapped.addAll(visited + options)
+        return false
     }
-    return visited to false
+
+    return map.all { it == '.' }.count { !checkEscape(it.key) }
 }
