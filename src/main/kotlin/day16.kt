ec4249fef3c106232316e9373afd12a74cf7267b
Gaute Meek Olsen <gaute.meek@gmail.com>
2023-12-24 00:07:23 +0100

    refactoring

--- src/main/kotlin/day16.kt
+++ src/main/kotlin/day16.kt
@@ -1,6 +1,4 @@
-import shared.Input
-import shared.Point
-import shared.XYMap
+import shared.*
 
 fun main() {
     val input = Input.day(16)
@@ -10,61 +8,54 @@ fun main() {
 
 fun day16A(input: Input): Int {
     val map = XYMap(input) { it }
-    return tilesEnergized(map, BeamState(Point(0, 0), "right"))
+    return tilesEnergized(map, Mover(Point(0, 0), Right))
 }
 
 fun day16B(input: Input): Int {
     val map = XYMap(input) { it }
-    val entries = map.all().filter { it.key.x == 0 }.map { BeamState(it.key, "right") } +
-            map.all().filter { it.key.x == map.width - 1 }.map { BeamState(it.key, "left") } +
-            map.all().filter { it.key.y == 0 }.map { BeamState(it.key, "down") } +
-            map.all().filter { it.key.y == map.height - 1 }.map { BeamState(it.key, "up") }
+    val entries = map.all().filter { it.key.x == 0 }.map { Mover(it.key, Right) } +
+            map.all().filter { it.key.x == map.width - 1 }.map { Mover(it.key, Left) } +
+            map.all().filter { it.key.y == 0 }.map { Mover(it.key, Down) } +
+            map.all().filter { it.key.y == map.height - 1 }.map { Mover(it.key, Up) }
     return entries.maxOf {
         tilesEnergized(map, it)
     }
 }
 
-private fun tilesEnergized(map: XYMap<Char>, start: BeamState): Int {
+private fun tilesEnergized(map: XYMap<Char>, start: Mover): Int {
     var beams = listOf(start)
     val history = beams.toMutableList()
     while (beams.isNotEmpty()) {
-        val nextBeams = mutableListOf<BeamState>()
+        val nextBeams = mutableListOf<Mover>()
         beams.forEach {
-            nextBeams.addAll(it.move(map[it.point]))
+            nextBeams.addAll(it.move(map[it.at]))
         }
-        beams = nextBeams.filter { !history.contains(it) && map.getOrNull(it.point) != null }
+        beams = nextBeams.filter { !history.contains(it) && map.getOrNull(it.at) != null }
         history.addAll(beams)
     }
-    return history.map { it.point }.toSet().size
+    return history.map { it.at }.toSet().size
 }
 
-private data class BeamState(val point: Point, val dir: String) {
-    fun move(on: Char): List<BeamState> {
-        return when {
-            (on == '.' || on == '|') && dir == "up" -> listOf(up())
-            (on == '.' || on == '-') && dir == "right" -> listOf(right())
-            (on == '.' || on == '|') && dir == "down" -> listOf(down())
-            (on == '.' || on == '-') && dir == "left" -> listOf(left())
-            on == '\\' && dir == "up" -> listOf(left())
-            on == '\\' && dir == "right" -> listOf(down())
-            on == '\\' && dir == "down" -> listOf(right())
-            on == '\\' && dir == "left" -> listOf(up())
-            on == '/' && dir == "up" -> listOf(right())
-            on == '/' && dir == "right" -> listOf(up())
-            on == '/' && dir == "down" -> listOf(left())
-            on == '/' && dir == "left" -> listOf(down())
-            on == '-' && (dir == "up" || dir == "down") -> {
-                listOf(left(), right())
-            }
-            on == '|' && (dir == "right" || dir == "left") -> {
-                listOf(up(), down())
-            }
-            else -> throw Exception()
+private fun Mover.move(on: Char): List<Mover> {
+    return when {
+        (on == '.' || on == '|') && dir == Up -> listOf(up())
+        (on == '.' || on == '-') && dir == Right -> listOf(right())
+        (on == '.' || on == '|') && dir == Down -> listOf(down())
+        (on == '.' || on == '-') && dir == Left -> listOf(left())
+        on == '\\' && dir == Up -> listOf(left())
+        on == '\\' && dir == Right -> listOf(down())
+        on == '\\' && dir == Down -> listOf(right())
+        on == '\\' && dir == Left -> listOf(up())
+        on == '/' && dir == Up -> listOf(right())
+        on == '/' && dir == Right -> listOf(up())
+        on == '/' && dir == Down -> listOf(left())
+        on == '/' && dir == Left -> listOf(down())
+        on == '-' && (dir == Up || dir == Down) -> {
+            listOf(left(), right())
         }
+        on == '|' && (dir == Right|| dir == Left) -> {
+            listOf(up(), down())
+        }
+        else -> throw Exception()
     }
-
-    private fun up() = BeamState(Point(point.x, point.y - 1), "up")
-    private fun right() = BeamState(Point(point.x + 1, point.y), "right")
-    private fun down() = BeamState(Point(point.x, point.y + 1), "down")
-    private fun left() = BeamState(Point(point.x - 1, point.y), "left")
 }
--- src/main/kotlin/shared/Point.kt
+++ src/main/kotlin/shared/Point.kt
@@ -1,8 +1,16 @@
 package shared
 
-data class Point(val x: Int, val y: Int) {
-    fun up() = Point(x, y - 1)
-    fun right() = Point(x + 1, y)
-    fun down() = Point(x, y + 1)
-    fun left() = Point(x - 1, y)
+data class Point(val x: Int, val y: Int)
+
+sealed interface Direction
+data object Up : Direction
+data object Right : Direction
+data object Down : Direction
+data object Left : Direction
+
+data class Mover(val at: Point, val dir: Direction, var steps: Int? = null) {
+    fun up() = Mover(Point(at.x, at.y - 1), Up, steps?.plus(1))
+    fun right() = Mover(Point(at.x + 1, at.y), Right, steps?.plus(1))
+    fun down() = Mover(Point(at.x, at.y + 1), Down, steps?.plus(1))
+    fun left() = Mover(Point(at.x - 1, at.y), Left, steps?.plus(1))
 }
