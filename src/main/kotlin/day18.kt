7bd69f3621afdbb811353101964838a0321b70ba
Gaute Meek Olsen <gaute.meek@gmail.com>
2023-12-25 12:17:35 +0100

    day18

--- src/main/kotlin/day18.kt
+++ src/main/kotlin/day18.kt
@@ -1,7 +1,5 @@
 import shared.*
 
-/* WARNING not cleaned up yet */
-
 fun main() {
     val input = Input.day(18)
     println(day18A(input))
@@ -9,63 +7,6 @@ fun main() {
 }
 
 fun day18A(input: Input): Long {
-    var mover = Mover(Point(0, 0), Right)
-    val path = mutableSetOf(mover)
-    input.lines.forEach {
-        val (direction, meters) = it.split(' ')
-        if(direction == "U") {
-            path.last().let { r ->
-                path.remove(r)
-                path.add(Mover(r.at, Up))
-            }
-        }
-        if(direction == "D") {
-            path.last().let { r ->
-                path.remove(r)
-                path.add(Mover(r.at, Down))
-            }
-        }
-        repeat(meters.toInt()) {
-            mover = when(direction) {
-                "U" -> mover.up()
-                "R" -> mover.right()
-                "D" -> mover.down()
-                "L" -> mover.left()
-                else -> throw Exception()
-            }
-            path.add(mover)
-        }
-    }
-    var paint = ""
-    val sumOnY = mutableMapOf<Int, Long>()
-    //var sum = 0
-    for(y in path.minOf { it.at.y } .. path.maxOf { it.at.y }) {
-        for(x in path.minOf { it.at.x } .. path.maxOf { it.at.x }) {
-            if(path.any { it.at == Point(x, y) }) {
-                //sum++
-                sumOnY[y] = (sumOnY[y] ?: 0) + 1
-                paint += if(path.first { it.at == Point(x, y) }.dir == Up) "U" else "#"
-            } else {
-                val leftWall = path.filter {
-                    it.at.y == y && it.at.x < x && (it.dir == Up || it.dir == Down)
-                }.maxByOrNull { it.at.x }
-                val rightWall = path.filter {
-                    it.at.y == y && it.at.x > x && (it.dir == Up || it.dir == Down)
-                }.minByOrNull { it.at.x }
-                val isInside = leftWall?.dir == Up && rightWall?.dir == Down
-                if(isInside) {
-                    //sum++
-                    sumOnY[y] = (sumOnY[y] ?: 0) + 1
-                    paint += "o"
-                } else {
-                    paint += "."
-                }
-            }
-        }
-        paint += "\n"
-    }
-    //println(paint)
-    //return sum*/
     var on = Point(0, 0)
     val vectors = input.lines.map { line ->
         val (direction, meters) = line.split(' ')
@@ -79,56 +20,7 @@ fun day18A(input: Input): Long {
             on = it.b
         }
     }
-    var sum = 0L
-    val minY = vectors.minOf { minOf(it.a.y, it.b.y) }
-    val maxY = vectors.maxOf { maxOf(it.a.y, it.b.y) }
-    for(y in minY .. maxY) {
-        println("$y - ${paint.lines()[y - minY]}")
-        val walls = vectors.filter {
-            it.a.y != it.b.y && y >= minOf(it.a.y, it.b.y) && y <= maxOf(it.a.y, it.b.y)
-        }.sortedBy { it.a.x }
-        var lineSum = 0L
-        val space = walls.zipWithNext { a, b ->
-            val aDirUp = a.a.y > a.b.y
-            val bDirUp = b.a.y > b.b.y
-            val isHorizontallyConnected = vectors.any {
-                it.a.y == y &&
-                        ((it.a.x == a.a.x && it.b.x == b.a.x) ||
-                                (it.b.x == a.a.x && it.a.x == b.a.x))
-            }
-            if((aDirUp && !bDirUp) || isHorizontallyConnected) {
-                a.a.x to b.a.x
-            } else {
-                null
-            }
-        }.filterNotNull()
-        sum += space.sumOf {
-            (it.second - it.first) + 1
-        } - space.count { a -> space.any { b -> a.first == b.second } }
-        lineSum += space.sumOf {
-            (it.second - it.first) + 1
-        } - space.count { a -> space.any { b -> a.first == b.second } }
-        /*walls.zipWithNext { a, b ->
-            val aDirUp = a.a.y > a.b.y
-            val bDirUp = b.a.y > b.b.y
-            val isHorizontallyConnected = vectors.any {
-                it.a.y == y &&
-                        ((it.a.x == a.a.x && it.b.x == b.a.x) ||
-                                (it.b.x == a.a.x && it.a.x == b.a.x))
-            }
-            if (aDirUp && !bDirUp) {
-                sum += (b.a.x - a.a.x) + 1
-                lineSum += (b.a.x - a.a.x) + 1
-            } else if(isHorizontallyConnected) {
-                sum += (b.a.x - a.a.x)
-                lineSum += (b.a.x - a.a.x)
-            }
-        }*/
-        if(lineSum != sumOnY[y]) {
-            println("verify ^")
-        }
-    }
-    return sum
+    return lagoonSize(vectors)
 }
 
 fun day18B(input: Input): Long {
@@ -146,14 +38,18 @@ fun day18B(input: Input): Long {
             on = it.b
         }
     }
+    return lagoonSize(vectors)
+}
+
+private class Vector(val a: Point, val b: Point)
+
+private fun lagoonSize(vectors: List<Vector>): Long {
     var sum = 0L
-    val minY = vectors.minOf { minOf(it.a.y, it.b.y) }
-    val maxY = vectors.maxOf { maxOf(it.a.y, it.b.y) }
-    for(y in minY .. maxY) {
-        val walls = vectors.filter {
+    for(y in vectors.minOf { minOf(it.a.y, it.b.y) } .. vectors.maxOf { maxOf(it.a.y, it.b.y) }) {
+        val wallsForRow = vectors.filter {
             it.a.y != it.b.y && y >= minOf(it.a.y, it.b.y) && y <= maxOf(it.a.y, it.b.y)
         }.sortedBy { it.a.x }
-        val space = walls.zipWithNext { a, b ->
+        val digged = wallsForRow.zipWithNext { a, b ->
             val aDirUp = a.a.y > a.b.y
             val bDirUp = b.a.y > b.b.y
             val isHorizontallyConnected = vectors.any {
@@ -167,11 +63,9 @@ fun day18B(input: Input): Long {
                 null
             }
         }.filterNotNull()
-        sum += space.sumOf {
+        sum += digged.sumOf {
             (it.second - it.first) + 1
-        } - space.count { a -> space.any { b -> a.first == b.second } }
+        } - digged.count { a -> digged.any { b -> a.first == b.second } }
     }
     return sum
 }
-
-private class Vector(val a: Point, val b: Point)
